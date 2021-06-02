8a88e122c29c977b449aaf2c8eeb7561f9c2f39a
Pilmax <guillaumemengelatte@hotmail.fr>
2021-06-02 16:33:39 +0200

    html/css Page Research

---
+++ vendor/assets/javascripts/spree/frontend/ActiveClass.js
@@ -0,0 +1,8 @@
+// var humburger_general = document.querySelector('.humburger_general');
+// var annimation_menu = document.querySelector('.annimation_menu');
+
+// humburger_general.onclick = function() {
+//     humburger_general.classList.toggle('active');
+//     annimation_menu.classList.toggle('active');
+
+// }
