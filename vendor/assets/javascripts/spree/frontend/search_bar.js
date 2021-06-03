39d9d0136ff3a27e3efdf2051749f8fdf4c10b44
NicoooM <nmarsan33000@gmail.com>
2021-06-03 17:34:45 +0200

    searchbar

--- vendor/assets/javascripts/spree/frontend/search_bar.js
+++ vendor/assets/javascripts/spree/frontend/search_bar.js
@@ -1,11 +1,28 @@
-$('.inputgroup').on('click',  () => {
-    $('.container').addClass('container-search-clicked')
-    $('.inputgroup').addClass('inputgroup-search-clicked')
-    $('.cross-btn').css('display', 'flex')
-  })
-  
-  $('.cross-btn').on('click', () => {
-    $('.container').removeClass('container-search-clicked')
-    $('.inputgroup').removeClass('inputgroup-search-clicked')
-    $('.cross-btn').css('display', 'none')
-  })
+function extendHeader() {
+  $("header").addClass("header-extend");
+  $(".cross-btn").css("display", "block")
+  $(".left-side-nav").css("display", "none")
+  $("#link-to-cart").css("display", "none")
+  $(".login").css("display", "none")
+  $(".container").css("padding-top", "30vh")
+  $("nav").css("padding-top", "15px")
+  $("#nav-bar").css("justify-content", "center")
+  $(".inputgroup").css("width", "40vw")
+  $(".blur").css("display", "block")
+}
+
+function reduceHeader() {
+  $("header").removeClass("header-extend");
+  $(".cross-btn").css("display", "none")
+  $(".left-side-nav").css("display", "inline-block")
+  $("#link-to-cart").css("display", "inline-block")
+  $(".login").css("display", "flex")
+  $(".container").css("padding-top", "0")
+  $("nav").css("padding-top", "0")
+  $(".inputgroup").css("width", "210px")
+  $(".blur").css("display", "none")
+}
+
+$(document).ready(function() {
+  $(".cart-info").text("Panier")
+})
