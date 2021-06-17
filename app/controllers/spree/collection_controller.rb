ef985023f29b773814388292ebc8b40e997a3731
NicoooM <nmarsan33000@gmail.com>
2021-06-17 11:55:04 +0200

    collections page

--- app/controllers/spree/collection_controller.rb
+++ app/controllers/spree/collection_controller.rb
@@ -1,6 +1,8 @@
 module Spree
   class CollectionController < Spree::StoreController
     def index
+      @taxonomies = Spree::Taxonomy.includes(root: :children)
+      @collection = Spree::Taxon.find_by(name: "Collections")
     end
   end
 end
