f6bfb98b3d176ca89ac8e441ba036a1e34a35a7c
Maxime Lasserre <lasserre.maxime@yahoo.fr>
2021-06-21 11:31:30 +0200

    support mailer

--- app/controllers/spree/support_controller.rb
+++ app/controllers/spree/support_controller.rb
@@ -1,6 +1,11 @@
 module Spree
   class SupportController < Spree::StoreController
     def index
+      @prenom = params[:prenom]
+      @nom = params[:nom]
+      @email = params[:email]
+      @sujet = params[:sujet]
+      @message = params[:message]
     end
   end
 end
