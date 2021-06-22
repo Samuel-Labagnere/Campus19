b2c05511f3a288641d728bc4b6a8226603f17a5f
Samuel-Labagnere <samuel.labagnere@mmibordeaux.com>
2021-06-22 14:21:50 +0200

    More pleasant looking Support mails

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,6 +5,6 @@ class ContactMailer < ApplicationMailer
         @sujet = params[:sujet]
         @message = params[:message]
 
-        mail(to: 'ecommerce.campus19@gmail.com', subject: '[SUPPORT] ' + @nom, body: '[' + @sujet + ']' + @message)
+        mail(to: 'ecommerce.campus19@gmail.com', subject: '[SUPPORT] ' + @nom, body: '[' + @sujet + '] ' + @message)
     end
 end
